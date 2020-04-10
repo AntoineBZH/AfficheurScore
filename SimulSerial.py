20aa4948eb4a514aebe10c03ca117e674339f7a9
Antoine <antoine.houssais.ah@gmail.com>
2020-04-10 19:36:00 +0200

    AHO : Ajout de la génération pseudo-aléatoire des trames de Stramatel

--- SimulSerial.py
+++ SimulSerial.py
@@ -3,6 +3,8 @@ import threading # Importe le module dédié à l'exécution parallèle
 import serial # Importe le module dédié aux liaisons série
 import io
 import time # Importe le module dédié à la gestion du temps
+import random # Importe le module dédié à l'aléatoire
+import math # Importe le module dédié au mathématiques
 
 class SimulSerial(threading.Thread):
     """Thread chargé de récupérer et mettre en forme le flux de donnée sur la liaison série"""
@@ -31,15 +33,65 @@ class SimulSerial(threading.Thread):
         self._SerialPort.open()
         if not self._SerialPort.is_open:
             raise SerialException("Impossible d\'instancier la classe Reader.")
-
+        
+        # Création des différents champs de la trame série
+        TimeNow = math.trunc(time.time())
+        self.LocalScore = 0
+        self.VisitorScore = 0
+        self.Time = "1500"
+        self.Period = 1
+        self.nbLocalPrison = random.randint(0, 3)
+        self.nbVisitorPrison = random.randint(0, 3)
+        
         # Création et initialisation du flux de sortie
-        self.outStream = bytes.fromhex(self._SerialPort.frameid) + " 0 318  0  010000000111222333    111222333         \r".encode('utf-8')
-
+        self.outStream = bytes.fromhex(self._SerialPort.frameid) + self.create_frame().encode('utf-8')
+        
     def run(self):
         """Exécution du thread"""
         while not self.StopIsAsked: # permet de quitter le thread en cours
+            if self.nbLocalPrison == 0 and self.nbVisitorPrison == 0:
+                self.nbLocalPrison = random.randint(0, 3)
+                self.nbVisitorPrison = random.randint(0, 3)
+            self.outStream = bytes.fromhex(self._SerialPort.frameid) + self.create_frame().encode('utf-8')
             self._SerialPort.write(self.outStream)
             time.sleep(0.1)
+    
+    def create_frame(self):
+        """Création de la trame à envoyer"""
+    
+        # Mise à jour des différents champs de la trame série
+        TimeNow = math.trunc(time.time())
+        
+        # Ajout au hasard de 1 au score
+        if (TimeNow % 120) == 100:
+            self.LocalScore += random.choice([0, 0, 0, 0, 1])
+            self.LocalScore %= 1000
+        if (TimeNow % 120) == 40:
+            self.VisitorScore += random.choice([0, 0, 0, 0, 1])
+            self.VisitorScore %= 1000
+        
+        self.Time = str(time.localtime(900 - (TimeNow % 900)).tm_min).rjust(2) + "{:02d}".format(time.localtime(900 - (TimeNow % 900)).tm_sec) # 15:00 représente 900 sec
+        if self.Time == ' 001':
+            self.Period = (self.Period % 2) + 1
+        
+        LocalPrison = ""
+        PrisonIter = 0
+        while PrisonIter < self.nbLocalPrison:
+            if (((3 - PrisonIter) * 60) - (TimeNow % ((3 - PrisonIter) * 60))) != 1:
+                LocalPrison += (str(time.localtime(((3 - PrisonIter) * 60) - (TimeNow % ((3 - PrisonIter) * 60))).tm_min) + "{:02d}".format(time.localtime(((3 - PrisonIter) * 60) - (TimeNow % ((3 - PrisonIter) * 60))).tm_sec))
+            PrisonIter +=1
+        self.nbLocalPrison = len(LocalPrison) // 3
+
+        VisitorPrison = ""
+        PrisonIter = 0
+        while PrisonIter < self.nbVisitorPrison:
+            if  (((3 - PrisonIter) * 60) - (TimeNow % ((3 - PrisonIter) * 60))) != 1:
+                VisitorPrison += (str(time.localtime(((3 - PrisonIter) * 60) - (TimeNow % ((3 - PrisonIter) * 60))).tm_min) + "{:02d}".format(time.localtime(((3 - PrisonIter) * 60) - (TimeNow % ((3 - PrisonIter) * 60))).tm_sec))
+            PrisonIter +=1
+        self.nbVisitorPrison = len(VisitorPrison) // 3
+        
+        return "{}{}{}{}{}{}{}{}{}{}{}{}".format(" 0", self.Time, str(self.LocalScore).rjust(3), str(self.VisitorScore).rjust(3),
+                        str(self.Period), str(self.nbLocalPrison), str(self.nbVisitorPrison), "00000", LocalPrison.ljust(9), "    ", VisitorPrison.ljust(9), "         \r")
         
     def __del__(self):
         """Ferme le port série à la fin du thread"""
@@ -48,5 +100,8 @@ class SimulSerial(threading.Thread):
             raise SerialException("Impossible de fermer le port {}.".format(self._SerialPort.port))
 
 if __name__ == "__main__":
-    TestSer = SimulSerial(port='COM2', frameid='30303035')
+    TestSer = SimulSerial(port='COM1', frameid='30303035')
     TestSer.start()
+    while True:
+        print(TestSer.outStream)
+        time.sleep(1)
